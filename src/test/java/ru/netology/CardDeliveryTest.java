cb4105ffc751596e74f045309a76a4e2ba8bbcf0
Alex Reshetnikova <voli-jast-merilin@mail.ru>
2021-01-31 22:46:39 +0600

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    DataGenerator dataGenerator = new DataGenerator();
+
+    String name = dataGenerator.getName();
+    String phone = dataGenerator.getPhone();
+    String city = dataGenerator.getCity();
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue(city);
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(dataGenerator.createDate(3));
+        form.$("[data-test-id=name] input").setValue(name);
+        form.$("[data-test-id=phone] input").setValue(phone);
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".notification_status_ok").shouldBe(exist);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на "+ dataGenerator.createDate(3)));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(dataGenerator.createDate(7));
+        form.$(".button").click();
+        $("[data-test-id=replan-notification]").waitUntil(exist, 15000);
+        $(withText("Перепланировать")).click();
+        $(".notification_status_ok").shouldBe(exist);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на "+ dataGenerator.createDate(7)));
+    }
+}
---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator{
+    private Faker faker;
+    private LocalDate today = LocalDate.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String createDate(int plusDays) {
+        LocalDate newDate = today.plusDays(plusDays);
+        return formatter.format(newDate);
+    }
+
+   public String getCity() {
+        String[] myCityList = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Владикавказ", "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Магас", "Махачкала", "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск", "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва", "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
+        int city = (int) Math.floor(Math.random() * myCityList.length);
+        return myCityList[city];
+    }
+
+
+    public String getName() {
+        faker = new Faker(new Locale("ru"));
+        return (faker.name().lastName()+(" ")+faker.name().firstName());
+    }
+
+    public String getPhone() {
+        faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
